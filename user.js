8f709b785e465674507dcf243c915cf4e34402a6
Frank Liang <frank97liang@gmail.com>
2017-05-20 23:01:23 -0400

    added user.js and portfolio.js

---
+++ user.js
@@ -0,0 +1,27 @@
+class User {
+	constructor() {
+		this.username = "Alice";
+		this.password = "123";
+		this.portfolio = new Portfolio();
+	}
+	
+	setUsername(uName){
+		if(uName != null){
+			this.username = uName;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+	
+	setPassword(pass){
+		if(pass != null){
+			this.password = pass;
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
