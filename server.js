1ecd00c056a45c468e155c8569dcbd861b896f70
Jian Ping Xu <jxu454@uwo.ca>
2017-05-22 12:47:08 -0400

    Got stock provider working.

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
