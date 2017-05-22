8687ab0f925132a8d7437740bdbce35c84e17f65
Jian Ping Xu <jxu454@uwo.ca>
2017-05-22 14:27:46 -0400

    Fixed link redirect.

--- server.js
+++ server.js
@@ -17,6 +17,10 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.get('/createUser', function(req, res){
+    res.sendFile(path.join(__dirname+ '/createUser.html'));
+})
+
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 });
