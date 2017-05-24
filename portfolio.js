03caaff6faefe46548f8aa04acb876b9b7bad8a3
Frank Liang <frank97liang@gmail.com>
2017-05-23 23:10:21 -0400

    Adding Portfolio.js logic

--- portfolio.js
+++ portfolio.js
@@ -1,8 +1,32 @@
 /**
  * Created by Frank Liang on 5/22/2017.
  */
-class Portfolio {
-	constructor() {
+function Portfolio() {
+	this.totalAssets = 1000;
+	this.investments = [];
+
+	this.addAssets=function(amount){
+		this.totalAssets += amount;
 	}
-	
+
+	this.addNewStock=function(stockId){
+		investments.push(stockId);
+	}
+
+	this.buyStocks = function(stockId, units){
+		//User doesn't have shares of this stock
+		if(this.investments.indexOf(stockId) < 0){
+
+		}
+	}
+
+	this.sellStocks=function(stockId, units){
+
+	}
+
+	this.getCurrentStockPrice=function(stockId){
+		return 1;
+	}
 }
+
+	
