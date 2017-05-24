6e0fbf43e6c39b0a34033ff0d2678f32a29ee656
Jian Ping Xu <jxu454@uwo.ca>
2017-05-23 23:09:39 -0400

    Setup POST request for adding a user.

--- userDb.js
+++ userDb.js
@@ -53,7 +53,7 @@ exports.findByUserName = function(req, res) {
 exports.addUsers = function(req, res) {
     var users = req.body;
     console.log('Adding user: ' + JSON.stringify(users));
-    db.collection('wines', function(err, collection) {
+    db.collection('users', function(err, collection) {
         collection.insert(users, {safe:true}, function(err, result) {
             if (err) {
                 res.send({'error':'An error has occurred'});
