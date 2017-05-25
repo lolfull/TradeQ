1ea0bbef46d000b20f43ee2e31ab586a2231c7c8
Frank Liang <frank97liang@gmail.com>
2017-05-24 23:39:43 -0400

    POST on backend

--- server.js
+++ server.js
@@ -9,17 +9,17 @@ var users = require(__dirname + '/userDb');
 
 app.get('/users', users.findAll);
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    next();
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/createUser', function(req, res){
+    res.sendFile(path.join(__dirname+ '/createUser.html'));
 });
 
 app.use(express.static(__dirname + '/'));
 
-app.use(bodyParser.urlencoded);
-
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 app.get('/users', users.findAll);
 
@@ -29,13 +29,6 @@ app.get('/users/username/:id', users.findByUserName);
 
 app.post('/users', users.addUsers);
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.get('/createUser', function(req, res){
-    res.sendFile(path.join(__dirname+ '/createUser.html'));
-});
 
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
